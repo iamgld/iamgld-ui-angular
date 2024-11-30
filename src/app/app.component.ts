425d18c0a9930d3ce26c0a42cd66f321be7f4207
iamgld <contact@iamgld.dev>
2024-11-30 18:34:12 -0300

    chore: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// Angular Imports
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
+  selector: 'gld-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'iamgld-ui-angular';
+  title = 'iamgld-ui-angular'
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// Angular Imports
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import {
+  provideClientHydration,
+  withEventReplay,
+  withIncrementalHydration,
+} from '@angular/platform-browser'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideHttpClient, withFetch } from '@angular/common/http'
+// This Component Imports
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
-};
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay(), withIncrementalHydration()),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+  ],
+}
