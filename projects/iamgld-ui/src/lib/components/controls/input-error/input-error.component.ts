3f918a994b5cd98ec120a45736594915b0c0202c
iamgld <contact@iamgld.dev>
2025-01-12 20:10:10 -0300

    feat(iamgld-ui): ✨ enhance input-error component to support default error messages and content projection

--- projects/iamgld-ui/src/lib/components/controls/input-error/input-error.component.ts
+++ projects/iamgld-ui/src/lib/components/controls/input-error/input-error.component.ts
@@ -1,11 +1,22 @@
 // Angular Imports
-import { booleanAttribute, ChangeDetectionStrategy, Component, input } from '@angular/core'
+import { NgTemplateOutlet } from '@angular/common'
+import {
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  contentChildren,
+  signal,
+  effect,
+} from '@angular/core'
 import { ValidationErrors } from '@angular/forms'
+// This Module Imports
+import { SelectInputErrorMessageDirective } from '../../../directives'
 
 @Component({
   selector: 'gld-input-error',
   standalone: true,
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './input-error.component.html',
   styleUrl: './input-error.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,4 +25,13 @@ export class InputErrorComponent {
   readonly Boolean = Boolean
   errors = input.required<ValidationErrors | null>()
   border = input<boolean, boolean | string>(false, { transform: booleanAttribute })
+  content = contentChildren(SelectInputErrorMessageDirective, {
+    descendants: true,
+  })
+
+  hasContent = signal(false)
+
+  constructor() {
+    effect(() => this.hasContent.set(this.content()?.length ? true : false))
+  }
 }
