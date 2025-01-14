ee45bf8b2aee7d6949e1c6ebf341de45eaa824a6
iamgld <contact@iamgld.dev>
2025-01-14 20:46:03 -0300

    feat(iamgld-ui): ✨ add TileComponent with styles and tests

--- projects/iamgld-ui/src/public-api.ts
+++ projects/iamgld-ui/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of iamgld-ui
  */
 
-export * from './lib/components'
-export * from './lib/validators'
 export * from './lib/utils'
+export * from './lib/validators'
 export * from './lib/directives'
+export * from './lib/components'
